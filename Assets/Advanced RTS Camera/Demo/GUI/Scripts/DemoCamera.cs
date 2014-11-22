ed6b9efbef348a4e7370f97f8415363fd15ad5e2
Barabicus <fiachra14@gmail.com>
2014-11-22 04:29:17 +0000

    some changes

---
+++ Assets/Advanced RTS Camera/Demo/GUI/Scripts/DemoCamera.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DemoCamera : MonoBehaviour {
+
+    public RTSCamera rtsCamera;
+    public InputField[] directionInps;
+
+    public bool Vertical
+    {
+        get { return rtsCamera.verticalSetup == RTSCamera.ControlSetup.Axis; }
+        set
+        {
+            if (value)
+                rtsCamera.verticalSetup = RTSCamera.ControlSetup.Axis;
+            else
+                rtsCamera.verticalSetup = RTSCamera.ControlSetup.Disabled;
+        }
+    }
+
+    public bool Horizontal
+    {
+        get { return rtsCamera.horizontalSetup == RTSCamera.ControlSetup.Axis; }
+        set
+        {
+            if (value)
+                rtsCamera.horizontalSetup = RTSCamera.ControlSetup.Axis;
+            else
+                rtsCamera.horizontalSetup = RTSCamera.ControlSetup.Disabled;
+        }
+    }
+
+    public bool Rotate
+    {
+        get { return rtsCamera.rotateSetup == RTSCamera.ControlSetup.Axis; }
+        set
+        {
+            if (value)
+                rtsCamera.rotateSetup = RTSCamera.ControlSetup.KeyCode;
+            else
+                rtsCamera.rotateSetup = RTSCamera.ControlSetup.Disabled;
+        }
+    }
+
+    public bool Tilt
+    {
+        get { return rtsCamera.tiltSetup == RTSCamera.ControlSetup.Axis; }
+        set
+        {
+            if (value)
+                rtsCamera.tiltSetup = RTSCamera.ControlSetup.KeyCode;
+            else
+                rtsCamera.tiltSetup = RTSCamera.ControlSetup.Disabled;
+        }
+    }
+
+    public bool Orbit
+    {
+        get { return rtsCamera.orbitSetup == RTSCamera.ControlSetup.Axis; }
+        set
+        {
+            if (value)
+                rtsCamera.orbitSetup = RTSCamera.ControlSetup.Axis;
+            else
+                rtsCamera.orbitSetup = RTSCamera.ControlSetup.Disabled;
+        }
+    }
+
+    public void SetDirectionX()
+    {
+
+        float x = Mathf.Clamp(int.Parse(directionInps[0].value), -1, 1);
+        float y = Mathf.Clamp(int.Parse(directionInps[1].value), -1, 1);
+        float z = Mathf.Clamp(int.Parse(directionInps[2].value), -1, 1);
+
+        directionInps[0].value = x.ToString();
+        directionInps[1].value = y.ToString();
+        directionInps[2].value = z.ToString();
+
+
+        rtsCamera.directionToMove = new Vector3(x,y,z);
+    }
+
+}
---
+++ Assets/Advanced RTS Camera/Demo/GUI/Scripts/GUIVector3.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIVector3 : MonoBehaviour {
+
+    public InputField[] vectorInputs;
+
+    private Vector3 vectorValue;
+
+    public void ValidateVector()
+    {
+    }
+
+
+}
